22c06d0033961115adf7ad5892d13f2fbb358477
Billzify1013 <ckhajwan@gmail.com>
2026-05-06 03:47:05 +0530

    new chnages in sitemap

--- turf_project/urls.py
+++ turf_project/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 from bookings import auth_views
+
+# Import sitemaps from bookings
+from bookings.urls import sitemaps
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +15,5 @@ urlpatterns = [
     path('logout/', auth_views.logout_view, name='logout'),
     path('', include('bookings.urls')),
     path('api/', include('bookings.api_urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
